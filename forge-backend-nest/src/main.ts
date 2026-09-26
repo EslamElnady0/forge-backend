027f4b1e1483f59f4a4db3c6c00543ef073f26ca
EslamElnady0 <eslamelnady254@gmail.com>
2026-09-26 11:39:03 +0300

    feat: implement global logging and exception handling, enhance Prisma service, and add CreateTaskDto

--- forge-backend-nest/src/main.ts
+++ forge-backend-nest/src/main.ts
@@ -1,8 +1,23 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  await app.listen(3000);
 }
-void bootstrap();
+bootstrap();
