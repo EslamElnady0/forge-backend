922b0a0e845f88446d027486817b2d8e5ef32ca0
EslamElnady0 <eslamelnady254@gmail.com>
2026-09-26 10:07:46 +0300

    feat: add Prisma integration and create modules for users, projects, and tasks
    - Added Prisma client and service for database interactions.
    - Created UsersModule, ProjectsModule, and TasksModule with respective controllers and services.
    - Updated app module to include new modules.

--- forge-backend-nest/src/app.module.ts
+++ forge-backend-nest/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
